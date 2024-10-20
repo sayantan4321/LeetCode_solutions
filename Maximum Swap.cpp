0ff444d699b6e1b3b8219283f560013c43581bc2
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-10-21 01:07:07 +0530

    Update Maximum Swap.cpp
    You are given an integer num. You can swap two digits at most once to get the maximum valued number.
    
    Return the maximum valued number you can get.
    
     
    
    Example 1:
    
    Input: num = 2736
    Output: 7236
    Explanation: Swap the number 2 and the number 7.
    Example 2:
    
    Input: num = 9973
    Output: 9973
    Explanation: No swap.
     
    
    Constraints:
    
    0 <= num <= 10^8

--- Maximum Swap.cpp
+++ Maximum Swap.cpp
@@ -1,20 +1,29 @@
+T.C. -> O(N)
+S.C. -> O(N)
+    
 class Solution {
 public:
     int maximumSwap(int num) {
-        
         string str = to_string(num);
-        int maxNum = num;
-        for(int i=0; i<str.length(); i++){
-            for(int j=i+1; j<str.length(); j++){
-                int digit1 = str[i] - '0';
-                int digit2 = str[j] - '0';
-                if(digit1 < digit2 ){
-                    swap(str[i], str[j]);
-                }
-                maxNum = max(maxNum, stoi(str));
-                swap(str[i], str[j]); // **** swap back ****
+        int n = str.size();
+        vector<int> rightmax(n);
+        rightmax[n-1] = n-1;
+        
+        for(int i=n-2; i>=0; i--){
+            int rightmaxIdx = rightmax[i+1]; // stores the right max element
+            int rightmaxEle = str[rightmaxIdx];
+
+            rightmax[i] = (str[i] > rightmaxEle) ? i : rightmaxIdx; 
+        }
+        for(int i=0; i<n; i++){
+            int maxrightIdx = rightmax[i];
+            int maxrightEle = str[maxrightIdx];
+            if(str[i] < maxrightEle){
+                swap(str[i], str[maxrightIdx]);
+                return stoi(str);
             }
         }
-        return maxNum;
+        return num;
+        
     }
 };
