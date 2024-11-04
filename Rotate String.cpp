26dd1519e540e1379b99abf6ef71de1a7e300a0e
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-11-04 14:52:31 +0530

    Create Rotate String.cpp

---
+++ Rotate String.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+      
+        if(s.size() != goal.size()) return false;
+        for(int i=0; i<s.length(); i++){
+           rotate(s.begin(), s.begin()+1, s.end());
+           if(s==goal){
+             return true;
+           }
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        queue<char> q1, q2;
+        if(s.size() != goal.size()) return false;
+        for(int i=0; i<s.size(); i++){
+            q1.push(s[i]);
+        }
+        for(int i=0; i<goal.size(); i++){
+            q2.push(goal[i]);
+        }
+        int k= goal.size()-1;
+        while(k!=0){
+            char ch= q2.front();
+            q2.pop();
+            q2.push(ch);
+
+            if(q1==q2) return true;
+            k--;
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()) return false;
+        string check = s+s;
+        return check.find(goal) != string::npos; 
+        // The use of npos in the comparison is a way to check if the find function failed to find the substring, as it returns npos in such cases.
+    }
+};
