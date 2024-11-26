0cf7021e5d0a12fadb3637f5349a7ee161a8fbd8
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-11-26 18:12:48 +0530

    Update Defuse the Bomb.cpp
    You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
    
    To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
    
    If k > 0, replace the ith number with the sum of the next k numbers.
    If k < 0, replace the ith number with the sum of the previous k numbers.
    If k == 0, replace the ith number with 0.
    As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
    
    Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
    
     
    
    Example 1:
    
    Input: code = [5,7,1,4], k = 3
    Output: [12,10,16,13]
    Explanation: Each number is replaced by the sum of the next 3 numbers. The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. Notice that the numbers wrap around.
    Example 2:
    
    Input: code = [1,2,3,4], k = 0
    Output: [0,0,0,0]
    Explanation: When k is zero, the numbers are replaced by 0. 
    Example 3:
    
    Input: code = [2,4,9,3], k = -2
    Output: [12,5,6,13]
    Explanation: The decrypted code is [3+9, 2+3, 4+2, 9+4]. Notice that the numbers wrap around again. If k is negative, the sum is of the previous numbers.
     
    
    Constraints:
    
    n == code.length
    1 <= n <= 100
    1 <= code[i] <= 100
    -(n - 1) <= k <= n - 1

--- Defuse the Bomb.cpp
+++ Defuse the Bomb.cpp
@@ -1,3 +1,31 @@
+// Brute Force
+
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+
+        vector<int> result(n, 0); // initialize 
+        if (k == 0) {
+            return result;
+        }
+        for (int i = 0; i < n; i++) {
+            if (k < 0) {
+                for (int j = i - abs(k); j < i; j++) {
+                    result[i] += code[(j + n) % n];
+                }
+            } else {
+                for (int j = i + 1; j < i + k + 1; j++) {
+                    result[i] += code[j % n];
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Approach - Sliding Window
+ 
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
@@ -27,17 +55,5 @@ public:
             j++;
         }
         return result;
-        // for (int i = 0; i < n; i++) {
-        //     if (k < 0) {
-        //         for (int j = i - abs(k); j < i; j++) {
-        //             result[i] += code[(j + n) % n];
-        //         }
-        //     } else {
-        //         for (int j = i + 1; j < i + k + 1; j++) {
-        //             result[i] += code[j % n];
-        //         }
-        //     }
-        // }
-        // return result;
     }
 };
