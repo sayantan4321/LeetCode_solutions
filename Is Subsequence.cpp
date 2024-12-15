b7a51768ce0885071e6df8bb491dffc0206fd141
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-12-16 00:28:50 +0530

    Create Is Subsequence.cpp
    Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
    
    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
    
     
    
    Example 1:
    
    Input: s = "abc", t = "ahbgdc"
    Output: true
    Example 2:
    
    Input: s = "axc", t = "ahbgdc"
    Output: false
     
    
    Constraints:
    
    0 <= s.length <= 100
    0 <= t.length <= 10^4
    s and t consist only of lowercase English letters.

---
+++ Is Subsequence.cpp
@@ -0,0 +1,16 @@
+// Optimal
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+       int n = s.length();
+       int m = t.length();
+       int i=0, j=0;
+       while(j < m){
+         if(s[i] == t[j]){
+            i++;
+         }
+         j++;
+       } 
+       return i==n ? true:false;
+    }
+};
