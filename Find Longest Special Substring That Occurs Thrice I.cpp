d8e5bd50c21797ce5700ad0a7d0e1a40353abff8
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2024-12-17 23:58:33 +0530

    Update Find Longest Special Substring That Occurs Thrice I.cpp
    You are given a string s that consists of lowercase English letters.
    
    A string is called special if it is made up of only a single character. For example, the string "abc" is not special, whereas the strings "ddd", "zz", and "f" are special.
    
    Return the length of the longest special substring of s which occurs at least thrice, or -1 if no special substring occurs at least thrice.
    
    A substring is a contiguous non-empty sequence of characters within a string.
    
     
    
    Example 1:
    
    Input: s = "aaaa"
    Output: 2
    Explanation: The longest special substring which occurs thrice is "aa": substrings "aaaa", "aaaa", and "aaaa".
    It can be shown that the maximum length achievable is 2.
    Example 2:
    
    Input: s = "abcdef"
    Output: -1
    Explanation: There exists no special substring which occurs at least thrice. Hence return -1.
    Example 3:
    
    Input: s = "abcaba"
    Output: 1
    Explanation: The longest special substring which occurs thrice is "a": substrings "abcaba", "abcaba", and "abcaba".
    It can be shown that the maximum length achievable is 1.
     
    
    Constraints:
    
    3 <= s.length <= 50
    s consists of only lowercase English letters.

--- Find Longest Special Substring That Occurs Thrice I.cpp
+++ Find Longest Special Substring That Occurs Thrice I.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int maximumLength(string s) {
        int n = s.length();
-       map<pair<char, int>, int>mpp;
+       unordered_map<string, int>mpp;
 
        for(int i=0; i<n; i++){ // O(N)
-          char ch = s[i];
-          int len = 0;
+          string curr;
           for(int j = i; j<n; j++){ // O(N) 
-             if(s[j] == ch){
-                len++;
-                mpp[{ch, len}]++; 
+             if(curr.empty() || curr.back() == s[j]){
+                curr.push_back(s[j]);
+                mpp[curr]++; // here we are traversing the map again
              }
              else{
                 break;
@@ -19,42 +18,16 @@ public:
         }
         int result = 0;
         for(auto it: mpp){
-           // string str =  it.first;
-           int len = it.first.second;
-           int freq = it.second;
-           if(freq >= 3 && len > result){
-              result = len;
+           string str =  it.first;
+           int cnt = it.second;
+           if(cnt >= 3 && str.length() > result){
+              result = str.length();
            }
         } 
-        return result == 0? -1:result; //  O(N^2)
-
-    //    int n = s.length();
-    //    unordered_map<string, int>mpp;
-
-    //    for(int i=0; i<n; i++){ // O(N)
-    //       string curr;
-    //       for(int j = i; j<n; j++){ // O(N) 
-    //          if(curr.empty() || curr.back() == s[j]){
-    //             curr.push_back(s[j]);
-    //             mpp[curr]++; // here we are traversing the map again
-    //          }
-    //          else{
-    //             break;
-    //          }
-    //       }
-    //     }
-    //     int result = 0;
-    //     for(auto it: mpp){
-    //        string str =  it.first;
-    //        int cnt = it.second;
-    //        if(cnt >= 3 && str.length() > result){
-    //           result = str.length();
-    //        }
-    //     } 
-    //     return result == 0? -1:result; //  O(N^3)
+        return result == 0? -1:result; //  O(N^3)
     }
 };
-
+// Optimization by taking by char
 class Solution {
 public:
     int maximumLength(string s) {
@@ -84,30 +57,5 @@ public:
            }
         } 
         return result == 0? -1:result; //  O(N^2)
-
-    //    int n = s.length();
-    //    unordered_map<string, int>mpp;
-
-    //    for(int i=0; i<n; i++){ // O(N)
-    //       string curr;
-    //       for(int j = i; j<n; j++){ // O(N) 
-    //          if(curr.empty() || curr.back() == s[j]){
-    //             curr.push_back(s[j]);
-    //             mpp[curr]++; // here we are traversing the map again
-    //          }
-    //          else{
-    //             break;
-    //          }
-    //       }
-    //     }
-    //     int result = 0;
-    //     for(auto it: mpp){
-    //        string str =  it.first;
-    //        int cnt = it.second;
-    //        if(cnt >= 3 && str.length() > result){
-    //           result = str.length();
-    //        }
-    //     } 
-    //     return result == 0? -1:result; //  O(N^3)
     }
 };
