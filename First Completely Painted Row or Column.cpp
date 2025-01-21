1c130a2fe293f4b550cdf65fd3d09574c578d91f
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2025-01-22 01:38:21 +0530

    Update and rename renene to First Completely Painted Row or Column.cpp
    nn

---
+++ First Completely Painted Row or Column.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+       unordered_map<int, int> mpp;
+       int ans = INT_MAX;
+       for(int i=0; i<arr.size(); i++){
+          mpp[arr[i]] = i;
+       } 
+       for(int i=0; i<mat.size(); i++){
+          int temp = INT_MIN;
+          for(int j=0; j<mat[0].size(); j++){
+              if(mpp.find(mat[i][j]) != mpp.end()){
+                 temp = max(temp, mpp[mat[i][j]]);
+              }
+          }
+          ans = min(ans, temp);
+       }
+       for(int i=0; i<mat[0].size(); i++){
+          int temp = INT_MIN;
+          for(int j=0; j<mat.size(); j++){
+              if(mpp.find(mat[j][i]) != mpp.end()){
+                 temp = max(temp, mpp[mat[j][i]]);
+              }
+          }
+          ans = min(ans, temp);
+       }
+       return ans;
+    }
+};
