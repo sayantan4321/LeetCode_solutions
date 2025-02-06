8e803748ee703c56f40a1f0922166afac45c9e23
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2025-02-06 23:45:58 +0530

    Update Maximum Ascending Subarray Sum.cpp
    Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
    
    A subarray is defined as a contiguous sequence of numbers in an array.
    
    A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of size 1 is ascending.
    
     
    
    Example 1:
    
    Input: nums = [10,20,30,5,10,50]
    Output: 65
    Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
    Example 2:
    
    Input: nums = [10,20,30,40,50]
    Output: 150
    Explanation: [10,20,30,40,50] is the ascending subarray with the maximum sum of 150.
    Example 3:
    
    Input: nums = [12,17,15,13,10,11,12]
    Output: 33
    Explanation: [10,11,12] is the ascending subarray with the maximum sum of 33.
     
    
    Constraints:
    
    1 <= nums.length <= 100
    1 <= nums[i] <= 100

--- Maximum Ascending Subarray Sum.cpp
+++ Maximum Ascending Subarray Sum.cpp
@@ -1,1 +1,22 @@
-mnkngrk
+// Approach - 1
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int maxSum = 0;
+        for (int startIdx = 0; startIdx < nums.size(); startIdx++) {
+            int currentSubarraySum = nums[startIdx];
+
+            // Inner loop to check the next elements forming an ascending
+            for (int endIdx = startIdx + 1;
+                 endIdx < nums.size() && nums[endIdx] > nums[endIdx - 1];
+                 endIdx++) {
+                currentSubarraySum += nums[endIdx];
+            }
+
+            // Update maxSum if we find a larger ascending subarray sum
+            maxSum = max(maxSum, currentSubarraySum);
+        }
+
+        return maxSum;
+    }
+};
