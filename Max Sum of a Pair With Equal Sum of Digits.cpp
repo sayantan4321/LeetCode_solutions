71bdfdcfb4e3d5c6ef5d4ea49db8d3f4c3f19aeb
Sayantan Poddar <49310169+sayantan4321@users.noreply.github.com>
2025-02-14 23:59:23 +0530

    Create Max Sum of a Pair With Equal Sum of Digits.cpp

---
+++ Max Sum of a Pair With Equal Sum of Digits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int sumofDigits(int num){
+        int sum = 0;
+        while(num > 0){
+           int d = num%10;
+           sum = sum+d;
+           num = num/10;
+        }
+        return sum;
+    }
+    int maximumSum(vector<int>& nums) {
+        int maxSum = -1;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(sumofDigits(nums[i]) == sumofDigits(nums[j])){
+                    maxSum = max(maxSum, nums[i]+nums[j]);
+                }
+            }
+        }
+        return maxSum;
+    }
+};
