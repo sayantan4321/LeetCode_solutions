772b36f4ae1f2e8ba014db64b9971d6876ab3741
sayantan4321 <sayantanpoddar0800@gmail.com>
2025-03-09 23:59:48 +0530

    Create Find the numbers of winning players.cpp
    abc

---
+++ Find the numbers of winning players.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+        int winningPlayerCount(int n, vector<vector<int>>& pick) {
+            vector<vector<int>>arr(n,vector<int>(11,0));
+            int ans=0;
+            for(int i=0;i<pick.size();i++){
+                int player=pick[i][0];
+                int color=pick[i][1];
+                arr[player][color]++;
+            }
+            for(int i=0;i<n;i++){
+                for(int j=0;j<=10;j++){
+                    if(arr[i][j] >i){
+                        ans++;
+                        break;
+                    }
+                }
+            }
+            return ans;
+        }
+    };
